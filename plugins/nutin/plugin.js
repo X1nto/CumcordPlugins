dde64f5d9342d3e5b21594ee0ec539aab86396f6
X1nto <X1nto@users.noreply.github.com>
2022-01-01 13:39:22 +0000

    deploy: 6b1d9e538c2bcf5d1539c0a034f1328881afbb83

---
+++ plugins/nutin/plugin.js
@@ -0,0 +1,5 @@
+(function(){"use strict";var t=e=>{let n;return{onLoad(){n=cumcord.patcher.injectCSS(`
+                img {
+                    content:url("../assets/nitin.png");
+                }
+            `)},onUnload(){n()}}};return t})();
