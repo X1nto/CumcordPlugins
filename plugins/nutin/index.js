bd549724366ca83d0506c15130fa84d44f29dc6d
X1nto <burtkni@yandex.ru>
2022-01-01 19:35:19 +0400

    experimental message content replacing

--- plugins/nutin/index.js
+++ plugins/nutin/index.js
@@ -5,12 +5,20 @@ export default (_data) => {
   return {
     onLoad() {
       nitin = injectCSS(`
-                img {
-                    content:url("https://x1nto.github.io/CumcordPlugins/assets/nitin${Math.floor(
-                      Math.random() * 2
-                    )}.jpg");
-                }
-            `);
+        img {
+          content: url("https://x1nto.github.io/CumcordPlugins/assets/${Math.floor(Math.random() * 2)}.jpg");
+        }
+        .markup-2BOw-j.messageContent-2qWWxC {
+          text-indent: -9999px;
+          line-height: 0;
+        }
+        .markup-2BOw-j.messageContent-2qWWxC::after {
+          content: 'I like fetishizing lesbians';
+          text-indent: 0;
+          display: block;
+          line-height: initial;
+        }
+      `);
     },
 
     onUnload() {
