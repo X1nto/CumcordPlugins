ca693603a8ab4aa7d3e62dc1cb289d0871318490
X1nto <X1nto@users.noreply.github.com>
2022-01-08 21:21:21 +0000

    deploy: 302f28d9aae52c9401e5acf2af811f18f85171e7

--- plugins/nutin/plugin.js
+++ plugins/nutin/plugin.js
@@ -1,15 +1,5 @@
-(function(n){"use strict";var e=i=>{let t;return{onLoad(){t=n.injectCSS(`
+(function(n){"use strict";var o=i=>{let t;return{onLoad(){t=n.injectCSS(`
         img {
-          content: url("https://x1nto.github.io/CumcordPlugins/assets/nitin${Math.floor(Math.random()*2)}.png");
+          content: url("https://x1nto.github.io/CumcordPlugins/assets/nitin${Math.floor(Math.random()*3)}.png");
         }
-        .markup-2BOw-j.messageContent-2qWWxC {
-          text-indent: -9999px;
-          line-height: 0;
-        }
-        .markup-2BOw-j.messageContent-2qWWxC::after {
-          content: 'I like fetishizing lesbians';
-          text-indent: 0;
-          display: block;
-          line-height: initial;
-        }
-      `)},onUnload(){t()}}};return e})(cumcord.patcher);
+      `)},onUnload(){t()}}};return o})(cumcord.patcher);
